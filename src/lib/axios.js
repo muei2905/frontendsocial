5f4c2f5d9e988b3165be3b9e09eeb5cccd23b28e
Thien Hoa <hoa@gmail.com>
2025-04-23 22:04:59 +0700

    save

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "https://backendsocial-1.onrender.com" : "/api",
+    baseURL: "https://backendsocial-1.onrender.com",
     withCredentials: true,
 });
